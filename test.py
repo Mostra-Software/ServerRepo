d4370c19b02ecc157407777a192bc34cc84bd7c0
Theiby <berkay.yucel52@gmail.com>
2024-02-02 16:44:12 +0300

    Second Update

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 from flask import Flask
+import subprocess
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route("/postStart/", methods=['GET', 'POST'])
 def post_start():
-    return "Start"
+   print("deneme")
+   return "start"
+    
 
 @app.route("/postStop/", methods=['GET', 'POST'])
 def post_stop():
     return "Stop"
+
+@app.route("/postredLight/", methods=['GET', 'POST'])
+def post_redLight():
+
+    # Python dosyasını çalıştırmak için subprocess kullanımı
+    try:
+        subprocess.run(["python", "C:/Users/berka/Desktop/Server/deneme.py"])
+        return "redLight"
+    except Exception as e:
+        print("Error:", str(e))
+        return "Error"
 
 if __name__ == '__main__':
     # Uygulamayı 0.0.0.0 adresine yayınla ve portu belirt
